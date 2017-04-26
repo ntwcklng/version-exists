a4ffdb3176e374070caa48f2ee78890b4c4b58c1
ntwcklng <ntwcklng@gmail.com>
2017-04-26 11:08:39 +0200

    semis

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const npmName = require('npm-name');
-const fetch = require('node-fetch');
+const npmName = require('npm-name')
+const fetch = require('node-fetch')
 
 module.exports = (module, version) =>
   new Promise((resolve, reject) => {
@@ -7,13 +7,13 @@ module.exports = (module, version) =>
     return npmName(module)
       .then(isNoValid => {
         if (isNoValid)
-          throw new Error(`Error > Cannot find ${module} in the NPM registry`);
+          throw new Error(`Error > Cannot find ${module} in the NPM registry`)
 
         fetch(`http://registry.npmjs.org/${module}`)
           .then(res => res.json())
           .then(body => {
-            return resolve({}.hasOwnProperty.call(body.time, version));
-          });
+            return resolve({}.hasOwnProperty.call(body.time, version))
+          })
       })
-      .catch(err => reject(err.message));
-  });
+      .catch(err => reject(err.message))
+  })
