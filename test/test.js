5fde5c336451ca9b430ecffc017c16a97feafaa5
ntwcklng <ntwcklng@gmail.com>
2017-04-26 11:08:21 +0200

    deps

--- test/test.js
+++ test/test.js
@@ -1,23 +1,23 @@
-const test = require('ava');
+const test = require('ava')
 
-const versionExists = require('../');
+const versionExists = require('../')
 
 test('Valid Version', async t => {
   await versionExists('args', '2.6.0').then(exists => {
-    t.true(exists);
-  });
-});
+    t.true(exists)
+  })
+})
 
 test('Inalid Version', async t => {
   await versionExists('args', '99.6.0').then(exists => {
-    t.false(exists);
-  });
-});
+    t.false(exists)
+  })
+})
 
 test('Module does not exist', async t => {
   try {
-    await versionExists('sdgsgsfdgsrghghrshrt', '2.6.0');
+    await versionExists('sdgsgsfdgsrghghrshrt', '2.6.0')
   } catch (err) {
-    t.is(err, 'Error > Cannot find sdgsgsfdgsrghghrshrt in the NPM registry');
+    t.is(err, 'Error > Cannot find sdgsgsfdgsrghghrshrt in the NPM registry')
   }
-});
+})
